1a5b964f90c1547c88f73c313c233eb539d6707e
NoFiix <khuddan@gmail.com>
2025-11-10 11:21:32 +0100

    apport des commentaires

--- Voting.sol
+++ Voting.sol
@@ -46,22 +46,24 @@ contract Voting is Ownable {
         voters [_addr].isRegistered = true ;
     }
 
+// -------------------------------------------------------------------------------------------------
 // Partie sur les Propositions ---------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 
-    function proposals_Activation () public onlyOwner {
+    function proposals_Activation () public onlyOwner { // seul le propriétaire du contrat peut activer la sessoin des propositions
         require (Status != WorkflowStatus.ProposalsRegistrationStarted, "Les propositions sont deja ouvertes") ;
         Status = WorkflowStatus.ProposalsRegistrationStarted ;
         emit WorkflowStatusChange (WorkflowStatus.RegisteringVoters, WorkflowStatus.ProposalsRegistrationStarted) ; // je ne comprends pas l'utilite de ces event. Pourquoi en avoir besoin ?
         //isPropositionOpen = true ; 
     }
-    function proposals_Desactivation () public onlyOwner {
+    function proposals_Desactivation () public onlyOwner { // le propriétaire du contrat peut désactiver la session des propositions
         require (Status != WorkflowStatus.ProposalsRegistrationEnded, "Les propositions sont deja ferme") ;
         Status = WorkflowStatus.ProposalsRegistrationEnded ;
         emit WorkflowStatusChange (WorkflowStatus.ProposalsRegistrationStarted, WorkflowStatus.ProposalsRegistrationEnded) ;
         //isPropositionOpen = false ;
     }
 
-    function propositions (string memory voteText) public {
+    function propositions (string memory voteText) public { // fonction pour enregistrer une proposition
         //require (isPropositionOpen, "Les propositions sont pour le moment ferme") ;
         require (Status == WorkflowStatus.ProposalsRegistrationStarted, "Les propositions sont pour le moment ferme") ;
         require (voters[msg.sender].isRegistered == true, "vous n etes pas autorise a effectuer cette action") ;
@@ -72,23 +74,25 @@ contract Voting is Ownable {
         emit ProposalRegistered (proposalId) ; // puis on emet l'event
     }
 
+// -------------------------------------------------------------------------------------------------
 // Partie sur les Votes -----------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 
-    function vote_Activation () public onlyOwner {
+    function vote_Activation () public onlyOwner { // l'Owner peut activer la session des votes
         require (Status != WorkflowStatus.VotingSessionStarted, "Les votes sont deja ouverts") ;
         Status = WorkflowStatus.VotingSessionStarted ;
         emit WorkflowStatusChange (WorkflowStatus.ProposalsRegistrationEnded, WorkflowStatus.VotingSessionStarted) ;
         //isVoteOpen = true ;
     }
 
-    function vote_Desactivation () public onlyOwner {
+    function vote_Desactivation () public onlyOwner { // l'Owner peut désactiver la session des votes
         require (Status != WorkflowStatus.VotingSessionEnded, "Les votes sont deja ferme") ;
         Status = WorkflowStatus.VotingSessionEnded ;
         emit WorkflowStatusChange (WorkflowStatus.VotingSessionStarted, WorkflowStatus.VotingSessionEnded) ;
         //isVoteOpen = false ;
     }
 
-    function vote_Choice (uint _proposal_Choosen) public {
+    function vote_Choice (uint _proposal_Choosen) public { // fonction pour voter pour une proposition
         Voter storage v = voters[msg.sender]; // vu que ça se répète souvent, autant en créer une variable 
 
         require (Status == WorkflowStatus.VotingSessionStarted, "Les votes sont pour le moment ferme") ;
@@ -100,12 +104,19 @@ contract Voting is Ownable {
         proposals[_proposal_Choosen].voteCount += 1 ;
         emit Voted (msg.sender , proposalId);
     }
- 
+
+// -------------------------------------------------------------------------------------------------
+// Partie pour le décompte des votes ---------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
     function number_of_Proposals () public view returns (uint) {
         return proposalId ;
     }
 
-    function getWinner () public returns (uint, uint, string memory) {
+// -------------------------------------------------------------------------------------------------
+// Getter ------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
+
+    function getWinner () public returns (uint, uint, string memory) { // un getter pour récupérer le gagnant, l'id de la proposition, le nombre de vote et la proposition 
         require (winningProposalId == 0, "le gagnant a deja ete selectionne") ;
         require (Status == WorkflowStatus.VotingSessionEnded, "attention, les votes sont encore ouvert") ;
         uint maxVotes = 0 ;
