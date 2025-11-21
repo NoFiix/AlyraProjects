5f8dfe2aefc7c554dcd89f762c794a6d85ddae19
NoFiix <khuddan@gmail.com>
2025-11-21 22:38:36 +0100

    ajout de nouveau test solidity mais aussi en JS pour la première fois

--- contracts/Voting.t.sol
+++ contracts/Voting.t.sol
@@ -12,8 +12,6 @@ contract VotingTest {
         voting = new Voting();
     }
 
-    // :::::::::: Owner :::::::::: //
-
 
     // :::::::::: WorkflowStatus initialisation :::::::::: //
 
@@ -23,7 +21,9 @@ contract VotingTest {
         //console.log (actual, expected);
         require(actual == expected, "Initial status must be RegisteringVoters");    }
 
+
     // :::::::::: WorkflowStatus Change :::::::::: //
+    // On test toutes les fonctions d'initialisation de workflowStatus les uns après les autres 
 
     function test_WorkflowStatus_ProposalsRegistrationStarted() public {
         voting.startProposalsRegistering();
@@ -75,6 +75,7 @@ contract VotingTest {
         //console.log (actual, expected);
         require(actual == expected, "Current status must be VotesTallied");    }
     
+    
     // :::::::::: test_proposalsArray() :::::::::: //
 
     function test_ProposalGenesisAdded() public {
@@ -85,23 +86,61 @@ contract VotingTest {
         require(voting.getOneProposal(0).voteCount == 0, "GENESIS proposaldmust exist at index 0"); // and the number of votes should be 0
     }
 
-    
 
-    
+    // :::::::::: addVoter :::::::::: //
 
+    /*
+    // Uniquement l'Owner peut ajouter des voters avec addVoter
+    function test_OwnerCanAddVoter() public { 
+        address voter = address(this); // On définit une adresse à whitelist
+        voting.addVoter(voter); // Owner (address(this)) ajoute voter
+        require(voting.getVoter(voter).isRegistered == true,"Voter should be registered by owner"); // On vérifie que le votant est bien enregistré
+    }
+    */
+
+    // Lorsque l'Owner utilise addVoter, le workflowStatus doit être à RegisteringVoters, dans tous les autres état ça doit revert
+    function test_addVoter_WorkflowStatus() public {
+
+    // Cas 1 : état initial => devrait réussir
+    (bool success1,) = address(voting).call(abi.encodeWithSignature("addVoter(address)", address(1)));
+    require(success1, "addVoter should succeed in RegisteringVoters state");
+
+    // Cas 2 : proposer registration started => revert attendu
+    voting.startProposalsRegistering();
+    (bool success2,) = address(voting).call(abi.encodeWithSignature("addVoter(address)", address(2)));
+    require(!success2, "addVoter should revert in ProposalsRegistrationStarted");
+
+    // Cas 3 : proposer registration ended => revert attendu
+    voting.endProposalsRegistering();
+    (bool success3,) = address(voting).call(abi.encodeWithSignature("addVoter(address)", address(3)));
+    require(!success3, "addVoter should revert in ProposalsRegistrationEnded");
+
+    // Cas 4 : voting session started => revert attendu
+    voting.startVotingSession();
+    (bool success4,) = address(voting).call(abi.encodeWithSignature("addVoter(address)", address(4)));
+    require(!success4, "addVoter should revert in VotingSessionStarted");
+
+    // Cas 5 : voting session ended => revert attendu
+    voting.endVotingSession();
+    (bool success5,) = address(voting).call(abi.encodeWithSignature("addVoter(address)", address(5)));
+    require(!success5, "addVoter should revert in VotingSessionEnded");
+
+    // Cas 6 : votes tallied => revert attendu
+    voting.tallyVotes();
+    (bool success6,) = address(voting).call(abi.encodeWithSignature("addVoter(address)", address(6)));
+    require(!success6, "addVoter should revert in VotesTallied state");
+}
+
+    // L'Owner ne peut pas ajouter deux fois la même address en tant que voter
+    function test_addVoterCantAddTwoSameAddresses () public {
+        voting.addVoter(address(1));
+        (bool success, ) = address(voting).call(abi.encodeWithSignature("addVoter(address)", address(1)));
+        require(!success, "addVoter shouldn't add two times a same address");
+    }
 
 
 
     /*
-
-    // :::::::::: addVoter :::::::::: //
-
-    function test_OwnerCanAddVoter() public {
-        address voter = address(1); // On définit une adresse à whitelist
-        voting.addVoter(voter); // Owner (address(this)) ajoute voter
-        require(voting.getVoter(voter).isRegistered == true,"Voter should be registered by owner"); // On vérifie que le votant est bien enregistré
-    }
-
 
     // :::::::::: addProposal :::::::::: //
 
@@ -132,7 +171,6 @@ contract VotingTest {
     }
 
     */
-
 
 
     // :::::::::: setVote :::::::::: //
--- test/Voting.ts
+++ test/Voting.ts
@@ -3,15 +3,122 @@ import { network } from "hardhat";
 
 const { ethers } = await network.connect();
 
-describe("Counter", function () {
-  it("Should emit the Increment event when calling the inc() function", async function () {
-    const counter = await ethers.deployContract("Counter");
+describe("Voting", function () {
 
-    await expect(counter.inc()).to.emit(counter, "Increment").withArgs(1n);
+  let voting, owner, voter1, voter2, voter3, voter4;
+
+  beforeEach(async function (){
+    [owner, voter1, voter2, voter3, voter4] = await ethers.getSigners();
+    const Voting = await ethers.getContractFactory("Voting");
+    voting = await Voting.deploy();
+    await voting.waitForDeployment();
   });
 
+
+
+// ::::::::::::: GETTERS ::::::::::::: //
+
+  it("getVoter function should return a voters information for registered voter", async function () {
+    // 1. Enregistrer le votant
+    await voting.connect(owner).addVoter(voter1.address);
+    // 2. Récupérer les infos (voter1 doit appeler car onlyVoters)
+    const voterInfo = await voting.connect(voter1).getVoter(voter1.address);
+    // 3. Vérifier
+    expect(voterInfo.isRegistered).to.be.true;
+    expect(voterInfo.hasVoted).to.be.false;
+    expect(voterInfo.votedProposalId).to.equal(0);
+  });
+
+  it("getOneProposal function should return proposals informations for an id", async function () {
+    // 1. Ajouter le votant
+    await voting.connect(owner).addVoter(voter1.address);
+    // 2. Démarrer la session d'enregistrement des propositions
+    await voting.connect(owner).startProposalsRegistering();
+    // 3. Maintenant vous pouvez ajouter une proposition
+    await voting.connect(voter1).addProposal("Hello");
+    // 4. Récupérer et vérifier
+    const proposalInfo = await voting.connect(voter1).getOneProposal(1);
+    expect(proposalInfo.description).to.equal("Hello");
+    expect(proposalInfo.voteCount).to.equal(0);
+  });
+
+  it("Should revert if non-voter use the getVoter function", async function () {
+    await voting.connect(owner).addVoter(voter1.address);
+    await expect (voting.connect(owner).getVoter(voter1.address)
+    ).to.be.revertedWith("You're not a voter");
+  })
+
+  it("Should revert if non-voter use the getOneProposal function", async function () {
+    // 1. Ajouter voter1
+    await voting.connect(owner).addVoter(voter1.address);
+    
+    // 2. Démarrer l'enregistrement des propositions
+    await voting.connect(owner).startProposalsRegistering();
+    
+    // 3. voter1 ajoute une proposition
+    await voting.connect(voter1).addProposal("Proposition1");
+    
+    // 4. Vérifier que voter1 PEUT récupérer la proposition
+    const proposal = await voting.connect(voter1).getOneProposal(1); // ID 1 car 0 = GENESIS
+    expect(proposal.description).to.equal("Proposition1");
+    expect(proposal.voteCount).to.equal(0);
+    
+    // 5. Vérifier qu'un autre user (non-voter) NE PEUT PAS récupérer la proposition
+    await expect(voting.connect(owner).getOneProposal(1)
+    ).to.be.revertedWith("You're not a voter");
+  });
+
+
+
+// ::::::::::::: REGISTRATION ::::::::::::: // 
+
+  it("Should revert if non-owner uses addVoter function", async function () {
+    await expect(voting.connect(voter1).addVoter(voter1.address)
+    ).to.be.revertedWithCustomError(voting, "OwnableUnauthorizedAccount")
+    .withArgs(voter1.address);
+  });
+
+  it("Should emit the VoterRegistered event when calling the addVoter function", async function () {
+    await expect(voting.addVoter(voter1)).to.emit(voting, "VoterRegistered").withArgs(voter1);
+  });
+
+
+
+// ::::::::::::: PROPOSAL ::::::::::::: // 
+
+
+
+// ::::::::::::: VOTE ::::::::::::: //
+
+
+
+// ::::::::::::: STATE ::::::::::::: //
+
+  it("Should revert if non-owner uses startProposalsRegistering function", async function () {
+    await expect(voting.connect(voter1).startProposalsRegistering()
+    ).to.be.revertedWithCustomError(voting, "OwnableUnauthorizedAccount")
+    .withArgs(voter1.address);
+  });
+
+
+
+// ::::::::::::: THE WINNER IS ::::::::::::: //
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
   it("The sum of the Increment events should match the current value", async function () {
-    const counter = await ethers.deployContract("Counter");
     const deploymentBlockNumber = await ethers.provider.getBlockNumber();
 
     // run a series of increments
@@ -33,4 +140,9 @@ describe("Counter", function () {
 
     expect(await counter.x()).to.equal(total);
   });
+  */
+
+
 });
+
+
